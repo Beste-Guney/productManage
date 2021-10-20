adc3215dc2cbdcff3262f27f94f5b492cbbba326
Beste-Guney <63497613+Beste-Guney@users.noreply.github.com>
2021-10-20 11:29:30 +0300

    created accounts app

---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class UserGroup(models.Model):
+    group_name = models.CharField(max_length=500, null=False)
+
+
+class Permission(models.Model):
+    permission_name = models.CharField(max_length=250, null=False)
+
+
+class UserGroupPermission(models.Model):
+    permission = models.ForeignKey(Permission)
+    user_group = models.ForeignKey(UserGroup)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    user_group = models.ForeignKey(UserGroup)
+
+

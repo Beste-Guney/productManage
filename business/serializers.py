ed97f706784d0d2d337a0f56e42f1b99ee6a4d8d
Nuray Akar <58554599+ingeniararto@users.noreply.github.com>
2021-11-02 13:30:18 +0300

    serializers added

--- business/serializers.py
+++ business/serializers.py
@@ -5,17 +5,17 @@ from .models import ActionLog
 
 
 class ActionLogSerializer(serializers.ModelSerializer):
-    user = serializers.RelatedField(source='user', read_only=True)
+    '''user = serializers.RelatedField(source='user', read_only=True)
     action_type = serializers.CharField(max_length=255)
     url = serializers.CharField(max_length=255)
-    payload = serializers.CharField(max_length=255)
+    payload = serializers.CharField(max_length=255)'''
 
     class Meta:
         model = ActionLog
         fields = ('__all__')
 
 class CompanySerializer(serializers.ModelSerializer):
-    company_name = serializers.CharField(max_length=255)
+    '''company_name = serializers.CharField(max_length=255)
     adress = serializers.CharField(max_length=255)
     email = serializers.CharField(max_length=255)
     phone_number = serializers.CharField(max_length=12)
@@ -25,7 +25,7 @@ class CompanySerializer(serializers.ModelSerializer):
     date_joined = serializers.DateTimeField(auto_now_add=True)
     created_by = serializers.RelatedField(source='created_by', read_only=True)
     company_type = serializers.CharField(max_length=255)
-    is_active = serializers.BooleanField(default=True)
+    is_active = serializers.BooleanField(default=True)'''
 
     class Meta:
         model = Company
